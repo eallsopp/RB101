d6a477acebd9abe90310628fe7080f6b42a82da9
Erik Allsopp <erik.allsopp@gmail.com>
2020-06-03 15:19:21 +0000

    refactor with player total

--- Lesson_6/twenty_one.rb
+++ Lesson_6/twenty_one.rb
@@ -55,13 +55,11 @@ def display_hands(player_hand, dealer_hand)
 end
 
 def busted?(hand)
-  total(hand) > 21
+  hand > 21
 end
 
-def result_of_game(player_cards, dealer_cards)
-  player_total = total(player_cards)
-  dealer_total = total(dealer_cards)
-
+def result_of_game(player_total, dealer_total)
+  
   if player_total > 21
     :player_busts
   elsif dealer_total > 21
@@ -75,8 +73,8 @@ def result_of_game(player_cards, dealer_cards)
   end
 end
 
-def display_result(player_cards, dealer_cards)
-  results = result_of_game(player_cards, dealer_cards)
+def display_result(player_total, dealer_total)
+  results = result_of_game(player_total, dealer_total)
   case results
   when :player_busts
     prompt('You busted!  Dealer wins!')
@@ -98,6 +96,28 @@ def blackjack(player_hand, dealer_hand)
   elsif total(dealer_hand) == 21
     prompt 'Dealer has a blackjack.  Dealer wins!'
     true
+  end
+end
+
+def dealer_outcomes(dealer_total)
+  if dealer_total >= 17 && dealer_total <= 21
+    :dealer_stays
+  elsif dealer_total <= 16
+    :dealer_hits
+  else
+    busted?(dealer_total)
+  end
+end
+
+def dealer_messages(dealer_total, deck, dealer_hand)
+  messages = dealer_outcomes(dealer_total)
+  case messages
+  when :dealer_stays
+    prompt('Dealer stays.')
+    
+  when :dealer_hits
+    deal(deck, dealer_hand)
+    prompt("Dealer cards are now #{dealer_hand}.")
   end
 end
 
@@ -123,57 +143,56 @@ loop do
   deck = initialize_deck
   initial_deal(deck, player_hand, dealer_hand)
   display_hands(player_hand, dealer_hand)
+  player_total = total(player_hand)
+  dealer_total = total(dealer_hand)
 
-    unless blackjack(player_hand, dealer_hand)
-      loop do
-        loop do
-          answer = nil
+  unless blackjack(player_hand, dealer_hand)
+    loop do
+      answer = nil
 
-          loop do # looping structure for the player hitting
-            prompt('Player:')
-            prompt("Your total is #{total(player_hand)}.")
-            prompt('Would you like to (h)it or (s)tay?')
-            answer = gets.chomp
-            break if %w(h s).include?(answer) || busted?(player_hand)
-            prompt 'Sorry, that is an invalid answer.'
-          end
-
-          if answer == 'h'
-            deal(deck, player_hand)
-            prompt("Your cards are now #{player_hand}.")
-          end
-
-          break if answer == 's' || busted?(player_hand)
-        end
-
-        unless busted?(player_hand)
-          prompt("OK, you stay at #{total(player_hand)}.")
-          prompt('Dealer hits on 16 and below, stays on 17 and above.')
-          loop do
-            prompt("The dealer has #{total(dealer_hand)}.")
-            sleep(1)
-
-            if total(dealer_hand) >= 17 && total(dealer_hand) <= 21
-              prompt('Dealer stays.')
-              sleep(1)
-              break
-            elsif total(dealer_hand) <= 16
-              deal(deck, dealer_hand)
-              prompt "Dealer's cards are now #{dealer_hand}."
-              sleep(1)
-            else
-              busted?(dealer_hand)
-              break
-            end
-          end
-
-        prompt("Player has #{total(player_hand)} and Dealer has #{total(dealer_hand)}.")
-        end
-
-        result_of_game(player_hand, dealer_hand)
-        display_result(player_hand, dealer_hand)
-        break
+      loop do # looping structure for the player hitting
+        player_total = total(player_hand)
+        prompt('Player:')
+        prompt("Your total is #{player_total}.")
+        prompt('Would you like to (h)it or (s)tay?')
+        answer = gets.chomp
+        break if %w(h s).include?(answer) || busted?(player_total)
+        prompt 'Sorry, that is an invalid answer.'
       end
+
+      if answer == 'h'
+        deal(deck, player_hand)
+        prompt("Your cards are now #{player_hand}.")
+      end
+      
+      player_total = total(player_hand)
+      break if answer == 's' || busted?(player_total)
     end
+
+    unless busted?(player_total) 
+      prompt("OK, you stay at #{player_total}.")
+      prompt('Dealer hits on 16 and below, stays on 17 and above.')
+      sleep(1)
+      loop do # begins dealer loop
+        dealer_total = total(dealer_hand)
+        prompt("The dealer has #{dealer_total}.")
+        sleep(1)
+        
+        dealer_messages(dealer_total, deck, dealer_hand)
+        break if busted?(dealer_total) || dealer_total >= 17
+      end
+    
+      prompt("Player has #{player_total} and Dealer has #{dealer_total}.")
+
+      result_of_game(player_total, dealer_total)
+      sleep(1)
+      display_result(player_total, dealer_total)
+      sleep(1)
+    end
+  end
+  if busted?(player_total)
+    prompt("Your total is #{player_total}.")
+    display_result(player_total, dealer_total)
+  end
   break unless replay?
 end
